6f15b5dc886de96b3b48ab9501a1f999b85714ab
magistermaks <magistermaks@darktree.net>
2026-09-20 18:46:52 +0200

    Initial 1.21.4 port

--- src/main/java/net/darktree/led/mixin/ServerRecipeManagerMixin.java
+++ src/main/java/net/darktree/led/mixin/ServerRecipeManagerMixin.java
@@ -30,7 +30,7 @@ abstract public class ServerRecipeManagerMixin {
 			method = "prepare(Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)Lnet/minecraft/recipe/PreparedRecipes;",
 			at = @At(
 					value = "INVOKE",
-					target = "Lnet/minecraft/resource/JsonDataLoader;load(Lnet/minecraft/resource/ResourceManager;Ljava/lang/String;Lcom/mojang/serialization/DynamicOps;Lcom/mojang/serialization/Codec;Ljava/util/Map;)V",
+					target = "Lnet/minecraft/resource/JsonDataLoader;load(Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/resource/ResourceFinder;Lcom/mojang/serialization/DynamicOps;Lcom/mojang/serialization/Codec;Ljava/util/Map;)V",
 					shift = At.Shift.AFTER
 			)
 	)
